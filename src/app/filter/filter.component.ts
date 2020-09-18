b4594123cd47c66034d51dc5057bbd93beca91c1
ankitkatras2 <ankit.katras2@gmail.com>
2020-09-18 16:36:42 +0530

    filters

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
-
-  constructor() { }
+  val1: number = 50;
+  myFormGroup: FormGroup;
+  categories: any[] = [{ name: 'Accounting', key: 'A' }, { name: 'Marketing', key: 'M' }, { name: 'Production', key: 'P' }, { name: 'Research', key: 'R' }];
+  constructor(private fb: FormBuilder) {
+    this.myFormGroup = fb.group({
+      'collection': [],
+      'color': [],
+      'category': [],
+      'price': []
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  handleChange(e) {
+    //e.value is the new value
+  }
+
+  getForm() {
+    console.log(this.myFormGroup.value);
+  }
 }
